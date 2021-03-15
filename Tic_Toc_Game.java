f47b66fc7d43032d27d841bc70c355c2794cb06d
unknown <somulanagireddy123@gmail.com>
2021-03-15 10:24:56 +0530

    [Nagireddy]: added player selction logic

--- Tic_Toc_Game.java
+++ Tic_Toc_Game.java
@@ -1,18 +1,38 @@
-package tictactoe;
+import java.util.Scanner;
 
-public class Tic_Toc_Game
-{
-    public static char[] createBoard()
-    {
-        char[] board = new char[10];
-        for(int index = 0; index < board.length; index++)
-        {
-            board[index] = ' ';
+public class Tic_Toc_Game {
+    // UC-1
+    static char[] board = new char[10];
+    public static char player;
+    public static char computer;
+
+    public static void makeEmpty() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
         }
-	public static void main(String[] args)
-        {
-        char[] board = createBoard();
-    	}
+    }
 
+    public static void playerSelect() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Symbol you choose to with (X/O): ");
+        String choice = scan.next();
+        switch(choice) {
+            case "X":
+                player = 'X';
+                computer = 'O';
+                break;
+            case "O":
+                player = 'O';
+                computer = 'X';
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        makeEmpty();
+        playerSelect();
     }
 }
